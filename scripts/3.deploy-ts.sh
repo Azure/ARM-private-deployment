8ee1500d954a324fa52a8d0e1ad56cec81f05d2f
Sarah Craddock <sarahcra@microsoft.com>
2021-07-15 10:06:54 +0100

    Update scripts and pipeline to use separate RG for TS

--- scripts/3.deploy-ts.sh
+++ scripts/3.deploy-ts.sh
@@ -4,28 +4,34 @@ set -x
 exit_with_error() {
   echo -e "Usage: $0\n" \
     "flag value                                    description\n" \
-    "-r   {RESOURCE_GROUP}                         Name of the resource group containing the Template Spec.\n" \
-    "-l   {LOCATION}                               The location to use for deployments.\n" \
+    "-r   {RESOURCE_GROUP}                         Name of the resource group for the deployment.\n" \
+    "-l   {LOCATION}                               Location for the deployment.\n" \
+    "-t   {TEMPLATE_SPEC_RESOURCE_GROUP}           Name of the resource group containing the Template Spec.\n" \
     "-n   {TEMPLATE_SPEC_NAME}                     Name of the Template Spec to deploy\n" \
     "-v   {VERSION}                                Version number to use for template specs i.e. 1.0.\n" 1>&2
   exit 1
 }
 
-while getopts :r:l:v:n:h: option
+while getopts :r:v:n:h:t:l: option
 do
   case "${option}"
     in
       r) rg=${OPTARG};;
-      l) location=${OPTARG};;
       v) version=${OPTARG};;
       n) template_spec=${OPTARG};;
+      t) template_spec_rg=${OPTARG};;
+      l) location=${OPTARG};;
       h) exit_with_error;;
       :) exit_with_error;;
       *) exit_with_error;;
   esac
 done
 
-id=$(az ts show --name "$template_spec" --resource-group "$rg" --version "$version" --query "id" -o tsv)
+az group create \
+  --name "$rg" \
+  --location "$location"
+
+id=$(az ts show --name "$template_spec" --resource-group "$template_spec_rg" --version "$version" --query "id" -o tsv)
 
 az deployment group create \
   --resource-group "$rg" \
